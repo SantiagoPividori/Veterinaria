915609d8ca704a52de6ee29622a2bd1e57a425f1
SantiagoPividori <pividorisantiago@gmail.com>
2025-12-21 13:14:29 -0300

    feat(api): add ApiPaths constants for API versioning

---
+++ src/main/java/com/pividori/veterinaria/api/ApiPath.java
@@ -0,0 +1,9 @@
+package com.pividori.veterinaria.api;
+
+public final class ApiPath {
+
+    public static final String API_V1 = "/api/v1";
+    //Para que no se pueda crear una instancia de la clase.
+    private ApiPath() {}
+
+}
